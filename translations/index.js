87b6789950bd51044ece444211b278c1f619f90a
Anthony Okonta <okontaa@gmail.com>
2024-08-27 21:52:37 +0200

    initial commit

--- translations/index.js
+++ translations/index.js
@@ -1,17 +1,28 @@
 
-
 export const translations=[
     {
        language:'De',
        translation:[
         {
             key:'Hello',
-            value:'Hallo'
+            value:'Halllllllllo'
         },
         {
             key:'Good morning',
             value:'Guten Morgen'
         }
        ]
+    },{
+        language:'Fr',
+        translation:[
+         {
+             key:'Hello',
+             value:'Hallo'
+         },
+         {
+             key:'Good morning',
+             value:'Guten Morgen'
+         }
+        ]
     }
 ]
